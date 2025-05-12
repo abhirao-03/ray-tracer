71cfeb653fdb1965dd05c16288ba6cde7725536a
Abhijeet Rao <abhijeetraop@gmail.com>
2025-05-12 15:42:36 +0100

    added progress bar

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ int main() {
     cout << "P3\n" << image_width << " " << image_height << "\n255\n"; // printing out to 
 
     for (int j = 0; j < image_height; j++) {
+        std::clog << '\rScanlines remaining: ' << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; i++) {
             auto r = double(i) / (image_width - 1);
             auto g = double(j) / (image_height - 1);
@@ -22,6 +23,8 @@ int main() {
             cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
-
+    
+    std::clog << "\rDone.                 \n";
+    
     return 0;
 }
